ee67fb5ccfd2ac46ceed4d01e61f47deb00c0976
capitaharlock <rjj@proars.com>
2025-02-02 19:55:17 +0100

    be

---
+++ api/README.md
@@ -0,0 +1,4 @@
+## Debug run in windows
+cargo install cargo-watch
+cargo watch -x run
+--> will run the API at | INFO api: Listening on 0.0.0.0:9123
--- api/src/services/local.rs
+++ api/src/services/local.rs
@@ -2,6 +2,7 @@ use crate::error::{WalletError, WalletResult};
 use crate::models::*;
 use bitcoin::{Network, PrivateKey};
 use bitcoin::secp256k1::{Secp256k1, SecretKey};
+use bitcoin::key::CompressedPublicKey;
 use bitcoincore_rpc::{Auth, Client as RpcClient, RpcApi};
 use rand::thread_rng;
 
@@ -20,8 +21,7 @@ impl LocalWalletService {
 
     pub async fn create_wallet(&self, password: &str) -> WalletResult<KeyPair> {
         let wallet_name = format!("wallet_{}", password);
-
-        // Create RPC client with wallet name in path
+        
         let rpc_client = RpcClient::new(
             &format!("http://localhost:18332/wallet/{}", wallet_name),
             Auth::UserPass(
@@ -29,34 +29,28 @@ impl LocalWalletService {
                 "charms_rpc_password".to_string(),
             ),
         ).map_err(|e| WalletError::BitcoinError(e.to_string()))?;
-
-        // Try to create wallet first
+    
         match rpc_client.create_wallet(&wallet_name, None, None, None, None) {
             Ok(_) => (),
             Err(e) => {
-                // If wallet exists, try to load it
                 if !e.to_string().contains("Database already exists") {
                     return Err(WalletError::BitcoinError(e.to_string()));
                 }
             }
         }
-
+    
         let secret_key = SecretKey::new(&mut thread_rng());
-        let public_key = secret_key.public_key(&self.secp);
         let private_key = PrivateKey::new(secret_key, self.network);
-
-        let address = rpc_client
-            .get_new_address(None, None)
+        let compressed_pub_key = CompressedPublicKey::from_private_key(&self.secp, &private_key)
             .map_err(|e| WalletError::BitcoinError(e.to_string()))?;
-
-        let validated_address = address
-            .require_network(self.network)
-            .map_err(|_| WalletError::InvalidAddress("Not a testnet address".to_string()))?;
-
+    
+        let address = bitcoin::Address::p2wpkh(&compressed_pub_key, self.network);
+    
         Ok(KeyPair {
             private_key: private_key.to_string(),
-            public_key: public_key.to_string(),
-            address: validated_address.to_string(),
+            public_key: compressed_pub_key.to_string(),
+            address: address.to_string(),
         })
     }
+
 }
